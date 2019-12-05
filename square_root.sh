c425ff8cd77bb54167909345ee0db59c119b40ae
ResmedJuanNavas <Narixa0340@>
2019-12-05 16:42:22 +0000

    Update script

--- square_root.sh
+++ square_root.sh
@@ -4,7 +4,12 @@ end=30000000
 while [ $i -le $end ]; do
 	a=4
 	b=12
-	echo "scale=5; sqrt($a^2 + $b^2)"
+	c=$((a^2 + b^2))
+	c=$((a^2 + b^2))
+	c=$((a^2 + b^2))
+	c=$((a^2 + b^2))
+	c=$((a^2 + b^2))
+	c=$((a^2 + b^2))
 	i=$(($i+1))
 done
 
