2cdf54c2ef8c9668e4d2c066b81ea649cef2bdaa
ResmedJuanNavas <Narixa0340@>
2019-12-06 09:02:56 +0000

    Simplify script

--- square_root.sh
+++ square_root.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 i=2
-end=30000000
-while [ $i -le $end ]; do
-	a=4
-	b=12
-	c=$((a^2 + b^2))
-	c=$((a^2 + b^2))
-	c=$((a^2 + b^2))
-	c=$((a^2 + b^2))
-	c=$((a^2 + b^2))
-	c=$((a^2 + b^2))
+end_outer=30000000
+while [ $i -le $end_outer ]; do
+	j=2
+	end_inner=10
+	while [ $j -le $end_inner ]; do
+		a=4
+		b=12
+		c=$((a^2 + b^2))
+		j=$(($j+1))
+	done
 	i=$(($i+1))
 done
 
